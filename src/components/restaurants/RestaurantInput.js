1ae6ef85b444f83a246ae9cb518160cbd85f3859
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-12-12 15:52:50 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -23,13 +23,13 @@ class RestaurantInput extends Component {
   render() {
     return (
       <div>
-         <form onSubmit={(event) => this.handleOnSubmit(event)}>
+         <form onSubmit={(event) => this.handleOnSubmit(event)} >
           <input
             type="text"
             value={this.state.text}
             onChange={(event) => this.handleOnChange(event)} />
           <input type="submit" />
-      </form>
+        </form>
       </div>
     );
   }
